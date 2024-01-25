20d12160e09d5ccdb89208431dd6fbca47e7e0c8
Kylie Roenigk <kr702922@ohio.edu>
2024-01-24 19:14:22 -0500

    Adding C++ program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Kylie Roenigk>
+ *        Date: <Jan 24 2023>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
